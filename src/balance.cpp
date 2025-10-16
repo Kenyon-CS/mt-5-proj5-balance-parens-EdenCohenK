e268195a7822e41dda9b75823006485833a4da3b
Eden Cohen <cohen10@kenyon.edu>
2025-10-16 13:42:14 -0400

    submit

--- src/balance.cpp
+++ src/balance.cpp
@@ -8,6 +8,17 @@ static bool is_balanced_rec(const std::string& s, int idx, int open) {
     // - If '(' => recurse open+1
     // - If ')' => recurse open-1
     // - else ignore
+
+    if (open < 0) return false;
+    
+    if (idx == (int)s.size()) return (open == 0);
+
+    if (s[idx] != '(' && s[idx] != ')') return is_balanced_rec(s, idx + 1, open);
+
+    if (s[idx] == '(') return is_balanced_rec(s, idx + 1, open + 1);
+
+    if (s[idx] == ')') return is_balanced_rec(s, idx + 1, open - 1);
+
     return false;
 }
 
